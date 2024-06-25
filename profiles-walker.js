9e44565810ad3adb107020a9dad418428effdf69
YuriiM <mokruy9999@gmail.com>
2024-06-26 01:07:58 +0200

    profile walker

---
+++ profiles-walker.js
@@ -0,0 +1,209 @@
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const axios = require('axios')
+const fs = require('fs')
+const filePath = require('path')
+require('dotenv').config({ path: filePath.join(__dirname, '..', '.env') })
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + process.env.BEARER_TOKEN
+const profileId = process.env.TEMPORARY_PROFILE_ID
+
+async function runBrowser(taskData) {
+  console.log('Running browser...with task ' + taskData.id)
+
+  let profilesList = JSON.parse(taskData.profiles_list)
+  let workingProfile = taskData.profile
+  let lowerDelayLimit = taskData.lower_delay_limit
+  let upperDelayLimit = taskData.upper_delay_limit
+
+  const userDataDir = filePath.join(__dirname, '..', 'userDataDir', workingProfile.username)
+
+  if (fs.existsSync(userDataDir)) {
+    console.log('User data directory finded!')
+    console.log(userDataDir)
+  } else {
+    console.log('User data directory does not finded!')
+    //in this case - login process should be started whrere we create user data directory
+    return
+  }
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: userDataDir,
+    protocolTimeout: 60000,
+    args: [
+      // '--proxy-server=' + profile.proxy + ':' + profile.proxyPort,
+      '--disable-features=IsolateOrigins,site-per-process,SitePerProcess',
+      '--flag-switches-begin --disable-site-isolation-trials --flag-switches-end'
+    ]
+  })
+
+  puppeteer.use(StealthPlugin())
+
+  const viewPort = {
+    width: 1280 + Math.floor(Math.random() * 100),
+    height: 900 + Math.floor(Math.random() * 100),
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    isLandscape: false,
+    isMobile: false
+  }
+
+  console.log('Opening browser for', workingProfile.username)
+
+  const page = await browser.newPage()
+  await page.setViewport(viewPort)
+  await page.setUserAgent(workingProfile.userAgent)
+
+  // await page.authenticate({
+  //   username: profile.proxyLogin,
+  //   password: profile.proxyPassword
+  // })
+
+  const url = 'https://www.instagram.com/'
+  await page.goto(url, { waitUntil: 'domcontentloaded' })
+
+  console.log('Page loaded')
+
+  console.log('Pause started ' + Date.now())
+
+  await new Promise((r) => {
+    setTimeout(r, lowerDelayLimit + Math.floor(Math.random() * (upperDelayLimit - lowerDelayLimit)))
+  })
+  console.log('Pause finished' + Date.now())
+
+  console.log(profilesList)
+
+  if (Array.isArray(profilesList) && profilesList.length > 0) {
+    for (const profileLogin of profilesList) {
+      console.log('Go to page of # ' + profileLogin)
+      await page.goto(url + profileLogin, { waitUntil: 'domcontentloaded' })
+
+      console.log('Scrolling...')
+      await page.evaluate(() => {
+        window.scrollBy(0, Math.random() * 1000)
+      })
+      console.log('Scrolling finished')
+
+      let pauseAfterScrollingStarted = new Date()
+      console.log('Pause after scrolling started ' + pauseAfterScrollingStarted)
+      await new Promise((r) => {
+        setTimeout(
+          r,
+          lowerDelayLimit + Math.floor(Math.random() * (upperDelayLimit - lowerDelayLimit))
+        )
+      })
+      let pauseAfterScrollingFinished = new Date()
+      console.log('Pause after scrolling finished ' + pauseAfterScrollingFinished)
+
+      // TODO if page.title - not found, do not clkicking, and randorm make click, do not make click, make two click
+      // TODO detect count of followers and following, and save this data in histore (to analize account activity), check count of posts if  profilie is not private, check if profile is private, check avatar
+      // TODO change walking task status on running when task ongoig, and on finished when task finished
+      console.log('Clicking on the page...')
+      await page.evaluate(() => {
+        const x = Math.floor(Math.random() * window.innerWidth)
+        const y = Math.floor(Math.random() * window.innerHeight)
+        const element = document.elementFromPoint(x, y)
+        if (element) {
+          element.click()
+        }
+      })
+      console.log('Clicking finished')
+
+      const pageTitle = await page.title()
+      console.log(`Page title is: ${pageTitle}`)
+
+      let pauseAfterWorkWithProfileStarted = new Date()
+      console.log('Pause after work with profile started ' + pauseAfterWorkWithProfileStarted)
+      await new Promise((r) => {
+        setTimeout(
+          r,
+          lowerDelayLimit + Math.floor(Math.random() * (upperDelayLimit - lowerDelayLimit))
+        )
+      })
+      let pauseAfterWorkWithProfileFinished = new Date()
+
+      console.log('Pause after work with profile finished ' + pauseAfterWorkWithProfileFinished)
+      await logWalkerActivity(
+        taskData.id,
+        workingProfile.id,
+        workingProfile.username,
+        profileLogin,
+        pageTitle,
+        pauseAfterScrollingStarted,
+        pauseAfterScrollingFinished,
+        pauseAfterWorkWithProfileStarted,
+        pauseAfterWorkWithProfileFinished
+      ).then((r) => console.log(r))
+    }
+  } else {
+    console.log('Profile list is not an array')
+  }
+
+  const title = await page.title()
+  console.log(`Page title is: ${title}`)
+  await browser.close()
+}
+
+async function fetchDataAndExecuteTask(url) {
+  try {
+    const response = await axios.get(url, {
+      params: {
+        profile_id: profileId
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    throw error
+  }
+}
+
+async function handleResponse(response) {
+  console.log('Handling response...')
+  if (Array.isArray(response) && response.length > 0) {
+    for (const taskData of response) {
+      console.log('Handling task data...task # ' + taskData.id)
+      await runBrowser(taskData)
+    }
+  } else {
+    console.log('Response is not an array')
+  }
+  return 'Handling response finished!'
+}
+
+async function logWalkerActivity(
+  taskDataId,
+  workingProfileId,
+  worknigProfileUserName,
+  profileLogin,
+  pageTitle,
+  pauseAfterScrollingStarted,
+  pauseAfterScrollingFinished,
+  pauseAfterWorkWithProfileStarted,
+  pauseAfterWorkWithProfileFinished
+) {
+  try {
+    const response = await axios.post(process.env.HOST + process.env.WALKER_HISTORY_URL_PATH, {
+      walking_task_id: taskDataId,
+      working_profile_id: workingProfileId,
+      working_profile_username: worknigProfileUserName,
+      handled_profile_login: profileLogin,
+      page_title: pageTitle,
+      pause_after_scrolling_started: pauseAfterScrollingStarted,
+      pause_after_scrolling_finished: pauseAfterScrollingFinished,
+      pause_after_work_with_profile_started: pauseAfterWorkWithProfileStarted,
+      pause_after_work_with_profile_finished: pauseAfterWorkWithProfileFinished
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error pass activity data:', error)
+    throw error
+  }
+}
+
+fetchDataAndExecuteTask(process.env.HOST + process.env.WALKER_URL_PATH)
+  .then((response) => {
+    console.log('Task data is fetched!')
+    handleResponse(response).then((r) => console.log(r))
+  })
+  .catch(console.error)
